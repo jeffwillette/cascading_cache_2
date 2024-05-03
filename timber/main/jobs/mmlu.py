fixed qwen bug related to attention mask
added mmlu batched experiment
updated _cascade_batched to work with deepspeed

timber/main/jobs/mmlu.py
@@ -6,8 +6,10 @@ import transformers
 from datasets import load_dataset
 import tqdm
 import numpy as np
+import deepspeed
 
 from timber.utils import seed, get_bench
+from timber.main.jobs.pg19 import get_injection_policy
 
 MMLU_FORMAT = """> The following are multiple choice questions (with answers) about {subject_name}.
 
@@ -19,6 +21,27 @@ C. {choice_c}
 D. {choice_d}
 
 Answer:"""
+
+MMLU_FORMAT_TARGET_QUESTION = """> I want you to answer the following are multiple choice question about {subject_name}.
+
+{number}. {question}
+
+A. {choice_a}
+B. {choice_b}
+C. {choice_c}
+D. {choice_d}
+
+Answer:{answer_placeholder}"""
+
+MMLU_REVERSED = """I want you to answer a question, but first I will show you some examples of similar questions. The question I am interested in is:
+
+{original_question}
+
+The examples of similar questions are:
+
+{examples}
+
+I want to know the answer to question 1. The answer ({answer_placeholder}) to the question I am interested in is:"""
 
 MMLU_SUBJECTS = [
     'business_ethics',
@@ -81,7 +104,11 @@ MMLU_SUBJECTS = [
 ]
 
 
-def format_mmlu(question, number, subject_name):
+def format_mmlu(question,
+                number,
+                subject_name,
+                target_question=False,
+                answer_placeholder=""):
     """
     {'input': 'A "dished face" profile is often associated with',
     'A': 'a protruding mandible due to reactivation of the condylar cartilage by acromegaly.',
@@ -91,7 +118,17 @@ def format_mmlu(question, number, subject_name):
     'target': 'B'}
     """
 
-    return MMLU_FORMAT.format(
+    if not target_question:
+        return MMLU_FORMAT.format(
+            subject_name=subject_name,
+            number=number,
+            question=question['input'],
+            choice_a=question['A'],
+            choice_b=question['B'],
+            choice_c=question['C'],
+            choice_d=question['D'],
+        )
+    return MMLU_FORMAT_TARGET_QUESTION.format(
         subject_name=subject_name,
         number=number,
         question=question['input'],
@@ -99,12 +136,11 @@ def format_mmlu(question, number, subject_name):
         choice_b=question['B'],
         choice_c=question['C'],
         choice_d=question['D'],
+        answer_placeholder=answer_placeholder,
     )
 
 
-def exam_mmlu(model, tokenizer: transformers.PreTrainedTokenizer, text):
-    PROMPT_ALWAYS_FLASH = os.environ.get('PROMPT_ALWAYS_FLASH', '0') == '1'
-    LAST_DENSE = os.environ.get('LAST_DENSE', '0') == '1'
+def exam_mmlu(model, tokenizer: transformers.PreTrainedTokenizer, texts):
 
     def gather_token_ids(candidates):
         ids = []
@@ -126,98 +162,159 @@ def exam_mmlu(model, tokenizer: transformers.PreTrainedTokenizer, text):
     assert hasattr(model, 'config')
     assert hasattr(model.config, 'max_position_embeddings')
 
-    inputs = tokenizer([text],
-                       return_tensors='pt',
-                       max_length=model.config.max_position_embeddings,
-                       truncation=True)
-    # print(inputs.input_ids.shape)
-    seq_len = inputs.input_ids.shape[-1]
-    with torch.no_grad():
-        if PROMPT_ALWAYS_FLASH:
-            for m in model.modules():
-                if hasattr(m, 'attention_method'):
-                    m.tree_dense_queries = seq_len - 1
-        if LAST_DENSE:
-            for m in model.modules():
-                if hasattr(m, 'attention_method'):
-                    m.tree_last_dense_queries = -1
+    inputs = tokenizer(
+        texts,
+        return_tensors='pt',
+        max_length=model.config.max_position_embeddings,
+        truncation=True,
+        padding=True,
+    )
 
+    # print(inputs.input_ids.shape)
+    # inputs["input_ids"] = inputs["input_ids"][:, :50]
+    # inputs["attention_mask"] = inputs["attention_mask"][:, :50]
+    seq_len = inputs.input_ids.shape[-1]
+
+    seq_lens = inputs.attention_mask.sum(dim=-1)
+
+    with torch.no_grad():
         sink = True
         if sink:
-            input_ids = inputs["input_ids"]
+            input_ids = inputs["input_ids"].cuda()
+            output = model(input_ids, use_cache=False)
+            logits = output.logits
 
-            past_key_values = None
-            with tqdm.tqdm(range(input_ids.size(1))) as pbar:
-                for i in pbar:
-                    inp = input_ids[:, i:i + 1]
-                    output = model(
-                        inp,
-                        use_cache=True,
-                        past_key_values=past_key_values,
-                    )
-                    past_key_values = output.past_key_values
+            pred_indices = (~inputs["attention_mask"].bool()).sum(
+                dim=-1, keepdim=True) + 1
 
-            output = output.logits.softmax(dim=-1)
+            # these are the first outputs after the last of the prompt sequence
+            pred_indices = seq_len - pred_indices
+            pred_indices = pred_indices.unsqueeze(-1).repeat(
+                1, 1, logits.size(-1))
+
+            logits = torch.gather(logits.cpu(), 1, pred_indices)
+            output = logits.softmax(dim=-1)
 
         else:
             output = model(**inputs).logits
             output = torch.softmax(output, dim=-1)
 
-    prob_a = max([output[0, -1, token].item() for token in tokens_a])
-    prob_b = max([output[0, -1, token].item() for token in tokens_b])
-    prob_c = max([output[0, -1, token].item() for token in tokens_c])
-    prob_d = max([output[0, -1, token].item() for token in tokens_d])
-    probs = [('A', prob_a), ('B', prob_b), ('C', prob_c), ('D', prob_d)]
-    probs = list(sorted(probs, key=lambda x: x[1], reverse=True))
-    return probs, seq_len
+    out_probs = []
+    for i in range(output.size(0)):
+        prob_a = max([output[i, -1, token].item() for token in tokens_a])
+        prob_b = max([output[i, -1, token].item() for token in tokens_b])
+        prob_c = max([output[i, -1, token].item() for token in tokens_c])
+        prob_d = max([output[i, -1, token].item() for token in tokens_d])
+        probs = [('A', prob_a), ('B', prob_b), ('C', prob_c), ('D', prob_d)]
+        probs = list(sorted(probs, key=lambda x: x[1], reverse=True))
+        out_probs.append(probs)
+
+    return out_probs, seq_lens
 
 
-def evaluate_mmlu(args, model, tokenizer, subject_name):
-    dataset = load_dataset('lukaemon/mmlu', subject_name)
-
+def get_fewshots(dataset, subject_name, shift=1):
     few_shots = []
     for question in dataset['train']:
-        text = format_mmlu(question, len(few_shots) + 1, subject_name)
+        text = format_mmlu(question, len(few_shots) + shift, subject_name)
         choice = question['target']
         text += choice
         few_shots.append(text)
     for question in dataset['validation']:
-        text = format_mmlu(question, len(few_shots) + 1, subject_name)
+        text = format_mmlu(question, len(few_shots) + shift, subject_name)
         choice = question['target']
         text += choice
         few_shots.append(text)
     few_shots = few_shots[:20]
+    return few_shots
+
+
+def format_mmlu_reversed(question, subject_name, few_shots):
+    original_question = format_mmlu(
+        question,
+        1,
+        subject_name,
+        target_question=True,
+        answer_placeholder="<|YOUR ANSWER|>",
+    )
+
+    examples = "\n\n".join(few_shots)
+    truth = question['target']
+
+    text = MMLU_REVERSED.format(
+        original_question=original_question,
+        examples=examples,
+        answer_placeholder="<|YOUR ANSWER|>",
+    )
+    return text, truth
+
+
+def format_mmlu_plain(question, subject_name, few_shots):
+    text = format_mmlu(
+        question,
+        len(few_shots) + 1,
+        subject_name,
+        target_question=True,
+        answer_placeholder="",
+    )
+    truth = question['target']
+    text = "\n\n".join(few_shots + [
+        text,
+    ])
+    return text, truth
+
+
+def evaluate_mmlu(args, model, tokenizer, subject_name, reverse=False):
+    dataset = load_dataset('lukaemon/mmlu', subject_name)
+
+    few_shots = get_fewshots(dataset, subject_name, shift=2 if reverse else 1)
 
     t_start = time.time()
     results = []
     n_correct = 0
     seq_len_sum = 0
-    for question in tqdm.tqdm(dataset['test'],
-                              dynamic_ncols=True,
-                              leave=True,
-                              desc=subject_name):
-        text = format_mmlu(question, len(few_shots) + 1, subject_name)
-        truth = question['target']
-        text = "\n\n".join(few_shots + [
-            text,
-        ])
-        estimations, seq_len = exam_mmlu(model, tokenizer, text)
-        estimation = estimations[0][0]
-        correct = truth == estimation
-        # print(truth, estimations, seq_len)
-        if correct:
-            n_correct += 1
-        seq_len_sum += seq_len
-        results.append({
-            # 'text': text,
-            'truth': truth,
-            'estimations': estimations,
-            'estimation': estimation,
-            'correct': correct,
-            'seq_len': seq_len,
-        })
-    elapsed = time.time() - t_start
 
+    qanda = [
+        format_mmlu_reversed(question, subject_name, few_shots)
+        if reverse else format_mmlu_plain(question, subject_name, few_shots)
+        for question in dataset["test"]
+    ]
+
+    print("total len of questions and answers: ", len(qanda))
+    n = len(qanda) // args.batch_size
+
+    with tqdm.tqdm(range(n), dynamic_ncols=True, leave=True,
+                   desc=subject_name) as pbar:
+        for i in pbar:
+            input_slice = qanda[i * args.batch_size:(i + 1) * args.batch_size]
+
+            texts = [v[0] for v in input_slice]
+            truths = [v[1] for v in input_slice]
+
+            batch_estimations, seq_lens = exam_mmlu(model, tokenizer, texts)
+            # print(f"{truths=} {batch_estimations=}")
+
+            for estimations, truth, seq_len in zip(batch_estimations, truths,
+                                                   seq_lens):
+                estimation = estimations[0][0]
+                correct = truth == estimation
+
+                # print(truth, estimations, seq_len)
+                if correct:
+                    n_correct += 1
+                seq_len_sum += seq_len
+
+                results.append({
+                    # 'text': text,
+                    'truth': truth,
+                    'estimations': estimations,
+                    'estimation': estimation,
+                    'correct': correct,
+                    'seq_len': seq_len.item(),
+                })
+
+            pbar.set_description(f"acc: {n_correct / len(results)}")
+
+    elapsed = time.time() - t_start
     accuracy = (n_correct / len(results)) * 100
     avg_seq_len = seq_len_sum / len(results)
     print(
@@ -226,8 +323,8 @@ def evaluate_mmlu(args, model, tokenizer, subject_name):
 
     os.makedirs('./saves/llama_eval/mmlu/', exist_ok=True)
     json_path = f'./saves/llama_eval/mmlu/{subject_name}_{args.model}_{args.method}.json'
-    if args.method == 'timber':
-        json_path = f'./saves/llama_eval/mmlu/{subject_name}_{args.model}_{args.method}_bq{args.block_size_q}_bk{args.block_size_k}_k{args.k}.json'
+    if args.method == 'sink':
+        json_path = f'./saves/llama_eval/mmlu/{subject_name}_{args.model}_{args.method}_window_{args.window}_cascade_{args.cascades}_sinks_{args.sinks}_comment_{args.comment}_reverse_{reverse}.json'
 
     with open(json_path, 'w') as f:
         json.dump(
@@ -252,13 +349,29 @@ def evaluate_mmlu(args, model, tokenizer, subject_name):
 def job_mmlu(args, model, tokenizer, device):
     seed()
 
-    # evaluate_mmlu(args, model, tokenizer, 'anatomy')
-    # return
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
-    accuracies = []
+    model.model.setup_caches(args.world_size)
+    # model = model.cuda()
+    model = deepspeed.init_inference(
+        model,
+        tensor_parallel={"tp_size": args.world_size},
+        replace_with_kernel_inject=False,
+        dtype=args.infer_dtype,
+        injection_policy=get_injection_policy(args.model),
+    )
+
+    accuracies, reversed_accuracies = [], []
     for subjects in MMLU_SUBJECTS:
-        acc = evaluate_mmlu(args, model, tokenizer, subjects)
+        acc = evaluate_mmlu(args, model, tokenizer, subjects, reverse=False)
         accuracies.append(acc)
+
+        rev_acc = evaluate_mmlu(args, model, tokenizer, subjects, reverse=True)
+        reversed_accuracies.append(rev_acc)
 
     accuracy = np.array(accuracies).mean()
     print(f'MMLU AVG. ACC: {accuracy}')
+
+    rev_accuracy = np.array(reversed_accuracies).mean()
+    print(f'MMLU AVG. ACC: {rev_accuracy}')

timber/models/modeling_llama.py
@@ -600,7 +600,7 @@ class LlamaAttention(nn.Module):
 
         # sin and cos are specific to RoPE models; cache_position needed for the static cache
 
-        c = torch.zeros_like(hidden_states)
+        c = torch.zeros_like(query_states.transpose(1, 2).view(bsz, q_len, -1))
         # print(f"{query_states.size()=} {c.size()=}")
         for i in tqdm(
                 range(q_len),
@@ -652,7 +652,7 @@ class LlamaAttention(nn.Module):
             )
             # print(f"{i} {out.size()=}")
 
-            c[:, i] = out
+            c[:, i:i + 1] = out
 
         out = self.o(c)
 

timber/models/qwen/modeling_qwen2.py
@@ -429,7 +429,7 @@ class Qwen2Attention(nn.Module):
 
         # sin and cos are specific to RoPE models; cache_position needed for the static cache
 
-        c = torch.zeros_like(hidden_states)
+        c = torch.zeros_like(query_states.transpose(1, 2).view(bsz, q_len, -1))
         # print(f"{query_states.size()=} {c.size()=}")
         for i in tqdm(
                 range(q_len),
@@ -481,7 +481,7 @@ class Qwen2Attention(nn.Module):
             )
             # print(f"{i} {out.size()=}")
 
-            c[:, i] = out
+            c[:, i:i + 1] = out
 
         out = self.o(c)
 
@@ -1435,6 +1435,8 @@ class Qwen2Model(Qwen2PreTrainedModel):
                 past_key_values_length,
                 sliding_window=self.config.sliding_window,
             )
+        elif self.config._sinks > 0:
+            pass
         else:
             # 4d mask is passed through the layers
             attention_mask = _prepare_4d_causal_attention_mask(
@@ -1681,7 +1683,8 @@ class Qwen2ForCausalLM(Qwen2PreTrainedModel):
                 attention_mask = attention_mask[:, -max_cache_length:]
 
         position_ids = kwargs.get("position_ids", None)
-        if attention_mask is not None and position_ids is None:
+        if attention_mask is not None and position_ids is None and not isinstance(
+                past_key_values, SinkCache):
             # create position_ids on the fly for batch generation
             position_ids = attention_mask.long().cumsum(-1) - 1
             position_ids.masked_fill_(attention_mask == 0, 1)