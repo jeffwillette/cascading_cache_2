5fd4f617c228879c3a262fbf23aae9fabbf085b2
Jeff Willette <jrwillette88@gmail.com>
2024-09-12 14:43:24 +0900

    updated passkey script

--- test-passkey.bash
+++ test-passkey.bash
@@ -3,20 +3,24 @@
 model=invalid-model-name
 method=sink
 cascades=1
-while getopts m:d:g:c: flag
+window=32768
+batch_size=2
+while getopts m:d:g:c:w:b: flag
 do
     case "${flag}" in
         m) model=${OPTARG};;
         d) method=${OPTARG};;
         g) gpu=${OPTARG};;
         c) cascades=${OPTARG};;
+        w) window=${OPTARG};;
+        b) batch_size=${OPTARG};;
     esac
 done
 
 # passkey experiment
-WINDOW=32768
+WINDOW=$window
 SINKS=64
-BATCH_SIZE=2
+BATCH_SIZE=$batch_size
 HEAD_REDUCTION=max
 CASCADE_STRIDE=4096
 CASCADE_FUNC="pow2"
